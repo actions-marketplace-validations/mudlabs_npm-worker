641bd682aaec4af5d2176693e746983f6763d003
Sam Donald <32623552+mudlabs@users.noreply.github.com>
2020-09-29 16:20:12 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const fs = require("fs");
 const yaml = require("js-yaml");
+const core = require("@actions/core");
 const github = require("@actions/github");
+
+const worker_parth = "./.github/npmworker.yaml";
+const no_worker = () => console.log("Could not locate an 'npmworker.yaml' file");
+const isNonEmptyArray = obj => obj && Array.isArray(obj);
 
 (async function(){
   try {
+    if (!fs.exists(worker_parth)) return no_worker();
+    const file = async fs.promises.readFile(worker_parth, { encoding: "utf-8" });
+    const data = yaml.safeLoad(file);
+    
+    const modules_path = data.path || "./";
+    
+    if (isNonEmptyArray(data.install)) {}
+    if (isNonEmptyArray(data.update)) {}
+    if (isNonEmptyArray(data.uninstall)) {}
+    
   } catch (error) {
   }
 })();
