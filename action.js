ed0f52d6f9a1d524f10fb60ffd341cf09a23850c
Sam Donald <32623552+mudlabs@users.noreply.github.com>
2020-10-01 16:25:01 +1000

    Update action.js

--- action.js
+++ action.js
@@ -4,14 +4,18 @@ const yaml = require("js-yaml");
 const core = require("@actions/core");
                                               
 const worker_path = "./.github/npmworker.yaml";
+const current_path = "";// the checked out directory path the action is called from;
+
+const marketplace = "https://github.com/marketplace/activity/npm-worker";
+let activity = `[NPM Worker](${marketplace}) {{description}}\n\n{{install}}\n\n{{update}}\n\n{{uninstall}}\n`;
+
 const isNonEmptyArray = obj => obj && Array.isArray(obj);
 const no_worker = () => core.setFailed("Could not locate the 'npmworker.yaml' configuration file.");
-const current_path = "";// the checked out directory path the action is called from;
 
 (async function(){
   try {
-    if (!fs.exists(worker_parth)) return no_worker();
-    const file = async fs.promises.readFile(worker_parth, { encoding: "utf-8" });
+    if (!fs.existsSync(worker_parth)) return no_worker();
+    const file = await fs.promises.readFile(worker_parth, { encoding: "utf-8" });
     const data = yaml.safeLoad(file);
     
     // Should be relative to current_path
@@ -20,12 +24,20 @@ const current_path = "";// the checked out directory path the action is called f
     // cd into the node_modules_path
     await execa("cd", node_modules_path);
         
-    if (isNonEmptyArray(data.install)) data.install.forEach(async package => await execa("npm install", package));
+    if (isNonEmptyArray(data.install)) {
+      data.install.forEach(async package => await execa("npm install", package));
+    }
     
-    if (isNonEmptyArray(data.update)) data.update.forEach(async package => await execa("npm update", package));
+    if (isNonEmptyArray(data.update)) {
+      data.update.forEach(async package => await execa("npm update", package));
+    }
     
-    if (isNonEmptyArray(data.uninstall)) data.uninstall.forEach(async package => await execa("npm uninstall", package));
+    if (isNonEmptyArray(data.uninstall)) {
+      data.uninstall.forEach(async package => await execa("npm uninstall", package));
+    }
     
+    core.setOutput(activity);
+    return;
   } catch (error) {
     core.setFailed(error);
   }
