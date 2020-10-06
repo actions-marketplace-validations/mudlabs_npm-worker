299332d8120c4442335933206de065a78f130336
Sam Donald <32623552+mudlabs@users.noreply.github.com>
2020-10-06 15:18:09 +1100

    Update action.js

--- action.js
+++ action.js
@@ -107,10 +107,13 @@ const getWorkerConfigPath = workflow => {
     // Should be relative to current_path
     const node_modules_path = data.path || "./";
     const valid_node_modules_path = await fs.exists(node_modules_path);
+    console.log("valid_node_modules_path",valid_node_modules_path);
     const has_package_json = await fs.exists(`${node_modules_path}/package.json`);
+    console.log("has_package_json",has_package_json);
     if (!valid_node_modules_path) return core.setFailed(`The path for node_modules does not exist.`);
+    console.log("pre EXECA.COMMAND");
     if (!has_package_json) await execa.command(`cd ${node_modules_path} && npm init -y`);
-   
+    console.log("Post EXECA.COMMAND")
     console.log("PATHS", valid_node_modules_path, has_package_json);
     
     // run the requested shell commands
