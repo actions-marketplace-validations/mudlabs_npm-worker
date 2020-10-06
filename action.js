91803b5b57e255a713625d102e29d8558428f150
Sam Donald <32623552+mudlabs@users.noreply.github.com>
2020-10-06 16:34:50 +1100

    Update action.js

--- action.js
+++ action.js
@@ -110,7 +110,8 @@ const getWorkerConfigPath = workflow => {
     const has_package_json = fs.existsSync(`${node_modules_path}/package.json`);
 
     if (!valid_node_modules_path) return core.setFailed(`The path for node_modules does not exist.`);
-    if (!has_package_json) await execa.command('npm init -y').stdout.pipe(await fs.createWriteStream(`${node_modules_path}/package.json`));    
+    if (!has_package_json) await execa.command('npm init -y').stdout.pipe(((f,s,b) => console.log(f))());
+//     await fs.createWriteStream(`${node_modules_path}/package.json`));    
     return;
     
     // run the requested shell commands
