8c537b18c000b5373e9b2e7d5cdecde710567017
Sam Donald <32623552+mudlabs@users.noreply.github.com>
2020-10-08 12:21:38 +1100

    Update action.js

--- action.js
+++ action.js
@@ -5,132 +5,25 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const report = require("./report");
 
-const isNonEmptyArray = obj => obj && Array.isArray(obj);
-
-// Array.prototype.drop = function(start, end = this.length - start) { 
-//   this.splice(start, end);
-//   return this; 
-// }
-
-// const buildActivityReport = (install, update, uninstall) => {
-  
-//   const condenseStderr = item => {
-//     const packageExp = new RegExp(`'(${item.package}(?:@.+)?)'`);
-//     const err_array = item.stderr.split("\n");
-//     const code = err_array[0];
-//     const stderr = err_array
-//       .drop(0,1)
-//       .filter(value => value !== "")
-//       .map(value => value.replace(/^npm ERR! (?:\d+)?/, "").trim())
-//       .map(value => value.replace(packageExp, "`$1`"))
-//       .join(" ");
-
-//     return item.shortMessage !== ""
-//       ? `${code}\n${item.shortMessage}\n${stderr}`
-//       : `${code}\n${stderr}`;
-//   }
-  
-//   const setInstalledItem = item => {
-//     if (item.failed) {
-//       console.log(item.stdout)
-//       return `- ![failed] \`${item.package}\`\n  > ${condenseStderr(item)}\n`;
-//     } else {
-//       return item.stdout.split("\n")
-//         .filter(value => value !== "")
-//         .drop(2,2)
-//         .map(value => value.startsWith("+ ") ? value.replace(/(\+ )(\S+)/, `- ![success] \`$2\``) : `  > ${value}`)
-//         .join("\n");
-//     }
-//   }
-  
-//   const setUpdatedItem = item => {
-//     console.log(item)
-//     if (item.failed) {
-//       return `- ![failed] \`${item.package}\`\n  > ${condenseStderr(item)}\n`
-//     } else {
-//       return item.stdout.split("\n")
-//         .filter(value => value !== "")
-//         .drop(2,2)
-//         .map(value => value.startsWith("+ ") ? value.replace(/(\+ )(\S+)/, `- ![success] \`$2\``) : `  > ${value}`)
-//         .join("\n");
-//     }
-//   };
-  
-//   const setUninstalledItem = item => {
-//     if (item.failed) {
-//       return `- ![failed] \`${item.package}\`\n  > ${condenseStderr(item)}\n`;
-//     } else {
-//       const stdout_array = item.stdout.split("\n").filter(value => value !== "").drop(1,2).map(value => `  > ${value}`);
-//       const just_passed = stdout_array[0].startsWith("  > audited");
-//       const package_state = `- ![${just_passed ? "passed" : "success"}] \`${item.package}\`\n`;
-//       if (just_passed) stdout_array.unshift(`  > Package does not seem to have been _installed_.`);
-//       return package_state + stdout_array.join("\n");
-//     }
-//   };
-  
-//   const buildList = title => items => {
-//     if (items.length < 1) return "";
-//     const list = items.reduce((list, item) => {
-//       let listItem;
-//       switch (title) {
-//         case "Installed":
-//           listItem = setInstalledItem(item);
-//           break;
-//         case "Updated":
-//           listItem = setUpdatedItem(item);
-//           break;
-//         case "Uninstalled":
-//           listItem = setUninstalledItem(item);
-//           break;
-//       }
-//       return list += `${listItem}\n`;
-//     }, `### ${title}\n`) + `\n`;
-//     return list;
-//   };
-  
-//   const buildDescription = () => {
-//     const numberOfPackages = items => {
-//       const number = items.length;
-//       return `${number} package${number === 1 ? "" : "s"}`;
-//     }
-//     const tag = title => items => items.length > 0 ? `_${title}_ ${numberOfPackages(items)}` : "";
-    
-//     const installed = tag("install")(install);
-//     const updated = tag("update")(update);
-//     const uninstalled = tag("uninstall")(uninstall);
-//     const opperations = ([installed, updated, uninstalled]
-//                          .filter(item => item !== "")
-//                          .map((item, index, array) => array.length > 1 && index === array.length - 1 ? `and ${item}` : item)
-//                         ).join(", ");
-    
-//     return `An update to your configuration file requested [NPM Worker][marketplace] ${opperations}.\n`;
-//   }
-  
-//   const marketplace = "[marketplace]: https://github.com/marketplace/activity/npm-worker";
-//   const icon = "[icon]: https://github.com/mudlabs/npm-worker/raw/master/npm_worker_icon.png";
-//   const success = "[success]: https://via.placeholder.com/15/15f06e/000000?text=+";
-//   const failed = "[failed]: https://via.placeholder.com/15/f03c15/000000?text=+";
-//   const passed = "[passed]: https://via.placeholder.com/15/e6c620/000000?text=+";
-//   const sender = github.context.payload.sender;
-//   const requester = `Requested by [\`@${sender.login}\`](https://github.com/${sender.login})`;
-//   const commit = `Triggered by commit ${github.context.sha}`;
-//   const description = buildDescription();
-//   const installed = buildList("Installed")(install)
-//   const updated = buildList("Updated")(update)
-//   const uninstalled = buildList("Uninstalled")(uninstall);
-//   const header = `> [![icon]][marketplace]\n> ${requester}\n> ${commit}\n`;
-//   const footer = `${marketplace}\n${icon}\n${success}\n${failed}\n${passed}`;
-      
-//   const report = `${header}\n${description}\n${installed}\n${updated}\n${uninstalled}\n\n${footer}`;
-//   return report;
-// }
+const logActivityToIssue = activity => async number => {
+  try {
+    const log = await octokit.issues.createComment({
+      owner: github.context.payload.repository.owner.login,
+      repo: githib.context.payload.repository.name,
+      issue_number: number,
+      body: activity
+    });
+  } catch (error) {
+    console.warn("Log Activity:" error);
+  }
+}
 
 const cleanConfigurationFile = path => async data => {
   try {
     for (const prop in data) data[prop] instanceof Array ? data[prop].length = 0 : null;
     const file = await fs.promises.writeFile(path, data);
   } catch (error) {
-    console.log(error);
+    throw error;
   }
 };
 
@@ -211,7 +104,6 @@ const initJSON = async path => {
     const file = await fs.promises.readFile(worker_config_path, { encoding: "utf-8" });
     const data = yaml.safeLoad(file);
         
-    // Should be relative to current_path
     const node_modules_path = data.path || "./";
     const valid_node_modules_path = fs.existsSync(node_modules_path);
     const has_package_json = fs.existsSync(`${node_modules_path}/package.json`);
@@ -219,26 +111,16 @@ const initJSON = async path => {
     if (!valid_node_modules_path) return core.setFailed(`The path for node_modules does not exist.`);
     if (!has_package_json) await initJSON(node_modules_path);  
     
-    // run the requested shell commands
     const installed = await shell("install")(data.install)(node_modules_path);
     const updated = await shell("update")(data.update)(node_modules_path);
     const uninstalled = await shell("uninstall")(data.uninstall)(node_modules_path);
     const activity_to_report = installed.concat(updated, uninstalled).length > 0;
-    
-    console.log("REPORT", activity_to_report);
-    
+        
     if (activity_to_report) {
       const activity = report.buildActivityReport(installed, updated, uninstalled);
-      console.log(activity)
       core.setOutput(activity);
-      if (data.issue) {
-        const response = await octokit.issues.createComment({
-          owner: github.context.payload.repository.owner.login,
-          repo: github.context.payload.repository.name,
-          issue_number: data.issue,
-          body: activity
-        });
-      }
+      if (data.issue) await logActivityToIssue(activity)(data.issue);
+      if (data.clean) await cleanConfigurationFile(worker_config_path)(data);
     }
     
   } catch (error) {
